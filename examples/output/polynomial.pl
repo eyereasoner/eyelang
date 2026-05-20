645b380f09f877dce89dbbb95f5d2c9a50236b55
josd <josderoo@gmail.com>
2026-05-20 23:26:36 +0200

    add polynomial roots example

---
+++ examples/output/polynomial.pl
@@ -0,0 +1,16 @@
+triple(complex_quartic, allRootsVerified, true).
+triple(complex_quartic, polynomial, [[1, 0], [-9, -5], [14, 33], [24, -44], [-26, 0]]).
+triple(complex_quartic, reconstructedPolynomial, [[1, 0], [-9, -5], [14, 33], [24, -44], [-26, 0]]).
+triple(complex_quartic, reconstructionMatches, true).
+triple(complex_quartic, root, [0, 1]).
+triple(complex_quartic, root, [1, 1]).
+triple(complex_quartic, root, [3, 2]).
+triple(complex_quartic, root, [5, 1]).
+triple(real_quartic, allRootsVerified, true).
+triple(real_quartic, polynomial, [[1, 0], [-10, 0], [35, 0], [-50, 0], [24, 0]]).
+triple(real_quartic, reconstructedPolynomial, [[1, 0], [-10, 0], [35, 0], [-50, 0], [24, 0]]).
+triple(real_quartic, reconstructionMatches, true).
+triple(real_quartic, root, [1, 0]).
+triple(real_quartic, root, [2, 0]).
+triple(real_quartic, root, [3, 0]).
+triple(real_quartic, root, [4, 0]).
